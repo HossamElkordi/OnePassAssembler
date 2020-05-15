41a34f78c62d7c495f66924dfbd55464bf6072ba
Minashraf <minachraf@outlook.com>
2020-05-15 15:57:16 +0200

    Cmake added for CLion

---
+++ cmake-build-debug/CMakeFiles/onepassassembler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/onepassassembler.dir/Assembler/src/assembler.cpp.o"
+  "onepassassembler"
+  "onepassassembler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/onepassassembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
