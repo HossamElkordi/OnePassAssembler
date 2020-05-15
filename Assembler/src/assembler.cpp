474381ac2c628280e3bd861697f8f51ae64bbe59
Minashraf <minachraf@outlook.com>
2020-05-15 21:08:24 +0200

    PC fixed

--- Assembler/src/assembler.cpp
+++ Assembler/src/assembler.cpp
@@ -170,7 +170,7 @@ string ReadFile(string path)
                 advance(ilist, 1);
                 second = *ilist;
             }
-            PC+=3;
+            PC+=format;
             //myFunc("",container.at(0),first,second,PC);
         } //TODO warnings and errors
         else if(OPTAB.find(container.at(1))!=OPTAB.end())
@@ -185,7 +185,7 @@ string ReadFile(string path)
                 advance(ilist, 1);
                 second = *ilist;
             }
-            PC+=3;
+            PC+=format;
             //myFunc(container.at(0),container.at(1),first,second,PC);
         }
         else if(DIRECTIVES.find(container.at(1))!=DIRECTIVES.end())
